5c274c95d496f89fa26df9c0549c7d9b736a2a54
Md Mamun Mia <mamunahammedco59@gmail.com>
2024-05-19 00:52:28 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+(function ($) {
+    "use strict";
+    jQuery(document).ready(function ($) {
+
+        //--- Counter -----
+
+        $('.counter').counterUp({
+            delay: 10,
+            time: 1000
+        });
+
+
+
+    }); //---document-ready-----
+}(jQuery));
